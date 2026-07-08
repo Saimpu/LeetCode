7bfcf0d66500ccfa2fd4214bad00434446916dd6
Karthik_967 <karthiksaimpu8@gmail.com>
2026-07-08 19:19:33 +0530

    Time: 0 ms (100%), Space: 50.4 MB (28.44%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int i=0;
+        int j=citations.length-1;
+        int n=citations.length;
+        if(n<=1){
+            return citations[0]>0?1:0;
+        }
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            int papers = n-mid;
+            if(citations[mid]==papers){
+                return citations[mid];
+            }else if(citations[mid]<papers){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+        return n-i;
+    }
+}
