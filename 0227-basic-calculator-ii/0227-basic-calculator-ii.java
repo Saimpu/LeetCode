32c809cbdd5062c7213a6d2fd73ff31b329c8b7c
Karthik_967 <karthiksaimpu8@gmail.com>
2026-09-17 19:16:05 +0530

    Time: 26 ms (23.5%), Space: 48.1 MB (66.13%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,47 +1,31 @@
 class Solution {
     public int calculate(String s) {
-
         Stack<Integer> st = new Stack<>();
-
         int num = 0;
-        char op = '+';
-
-        for (int i = 0; i < s.length(); i++) {
-
+        char sign = '+';
+        for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
+            if(Character.isDigit(ch)){
+                num = num*10 + ch - '0';
             }
-
-            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
-
-                if (op == '+') {
+            if((ch!=' ' && !Character.isDigit(ch)) || i == s.length()-1){
+                if(sign == '+'){
                     st.push(num);
-                }
-
-                else if (op == '-') {
+                }else if(sign == '-'){
                     st.push(-num);
+                }else if(sign == '*'){
+                    st.push(num*st.pop());
+                }else{
+                    st.push(st.pop()/num);
                 }
-
-                else if (op == '*') {
-                    st.push(st.pop() * num);
-                }
-
-                else if (op == '/') {
-                    st.push(st.pop() / num);
-                }
-
-                op = ch;
-                num = 0;
+                sign = ch;
+                num =0;
             }
         }
-
-        int ans = 0;
-
-        while (!st.isEmpty()) {
-            ans += st.pop();
+        int result = 0;
+        while(!st.isEmpty()){
+            result+=st.pop();
         }
-
-        return ans;
+        return result;
     }
 }
